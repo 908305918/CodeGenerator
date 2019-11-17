756a9b01fb463e1f82a97057ff578055ff1de56c
y908305918 <908305918@qq.com>
2019-11-17 13:04:40 +0800

    modify

--- src/main/java/com/quick/generator/controller/CodeController.java
+++ src/main/java/com/quick/generator/controller/CodeController.java
@@ -21,7 +21,7 @@ public class CodeController {
     private CodeGenerator codeGenerator;
 
     @GetMapping("/init")
-    public void test(HttpServletResponse response) throws Exception {
+    public void init(HttpServletResponse response) throws Exception {
         ConfigBean config = new ConfigBean();
         config.setDsUrl("jdbc:mysql://localhost:3306/spring?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&failOverReadOnly=false&zeroDateTimeBehavior=convertToNull&useSSL=false");
         config.setDriverName("com.mysql.jdbc.Driver");
@@ -31,6 +31,28 @@ public class CodeController {
         config.setPackageName("com.quick.meeting");
         config.setTablePrefix("m_");
         config.setProjectName("meeting");
+
+        byte[] data = codeGenerator.execute(config, "m_meeting");
+
+        response.setHeader("Content-Disposition", "attachment; filename=\"out.zip\"");
+        response.addHeader("Content-Length", "" + data.length);
+        response.setContentType("application/octet-stream; charset=UTF-8");
+
+        IOUtils.write(data, response.getOutputStream());
+    }
+
+    @GetMapping("/initCode")
+    public void initCode(String dsUrl, String dsUsername, String dsPassword, String prefix,
+                         String packageName, String projectName, HttpServletResponse response) throws Exception {
+        ConfigBean config = new ConfigBean();
+        config.setDsUrl(dsUrl);
+        config.setDriverName("com.mysql.jdbc.Driver");
+        config.setUsername(dsUsername);
+        config.setPassword(dsPassword);
+        config.setRootPath(fileDir + "Code/");
+        config.setPackageName(packageName);
+        config.setTablePrefix(prefix);
+        config.setProjectName(projectName);
 
         byte[] data = codeGenerator.execute(config, "m_meeting");
 
